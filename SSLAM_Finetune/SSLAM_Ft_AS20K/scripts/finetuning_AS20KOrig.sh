12004aa88fd8d812da9c4b255cd088ab969b39f6
ta012 <tony123alex@gmail.com>
2025-06-03 17:38:03 +0100

    bash scripts update

--- SSLAM_Finetune/SSLAM_Ft_AS20K/scripts/finetuning_AS20KOrig.sh
+++ SSLAM_Finetune/SSLAM_Ft_AS20K/scripts/finetuning_AS20KOrig.sh
@@ -8,24 +8,23 @@ echo "batch_size: $batch_size"
 
 num_gpus=1
 
-mnt_vol=/mnt/fast/nobackup/users/ta01123/
-main_dir=${mnt_vol}/eat_apr30/fairseq/
+main_dir=/path/to/fairseq/
 echo "main_dir: $main_dir"
-eat_dirname=EAT_FtCkptMixRegionExtraMixLCls1Layer2S2TDrpInpTBothNoMixCls
+eat_dirname=SSLAM_Ft_AS20K
 exp_dir_name=$(date +"%d_%m_%H_%M_%S")
 echo "exp_dir_name name: $exp_dir_name"
 
 # optimizer.groups.default.lr_scheduler.min_lr=0.00005 \
 
-pretrained_weight=/mnt/fast/nobackup/users/ta01123/eat_apr30/fairseq/EAT_CkptMixRegionExtraMixLCls1Layer2S2TDrpInpTBothNoMixCls/experiments/EAT_CkptMixRegionExtraMixLCls1Layer2S2TDrpInpTBothNoMixCls/checkpoint_last.pt
+pretrained_weight=/path/to/SSLAM_Stage2/experiments/SSLAM_Stage2/checkpoint_last.pt
 
 
-/mnt/fast/nobackup/users/ta01123/eat_apr30/fairseqvenv/bin/python ${main_dir}/fairseq_cli/hydra_train.py -m \
+/path/to/fairseqvenv/bin/python ${main_dir}/fairseq_cli/hydra_train.py -m \
     --config-dir ${main_dir}/${eat_dirname}/config \
     --config-name finetuning  \
     common.user_dir=${eat_dirname} \
     hydra.sweep.dir=${main_dir}/${eat_dirname}/experiments/${exp_dir_name}_multirun/ \
-    checkpoint.save_dir=/mnt/fast/nobackup/scratch4weeks/ta01123/to_del_eat/${exp_dir_name}/ \
+    checkpoint.save_dir=${main_dir}/${eat_dirname}/experiments/${exp_dir_name}/ \
     checkpoint.restore_file=None \
     checkpoint.save_interval=500000 \
     checkpoint.best_checkpoint_metric=mAP \
@@ -33,7 +32,7 @@ pretrained_weight=/mnt/fast/nobackup/users/ta01123/eat_apr30/fairseq/EAT_CkptMix
     checkpoint.no_last_checkpoints=true \
     checkpoint.no_save_optimizer_state=true \
     dataset.batch_size=${batch_size} \
-    task.data=${mnt_vol}/eat_data_manifest/manifest_as20k/ \
+    task.data=${main_dir}/eat_data_manifest/manifest_as20k/ \
     task.target_length=1024 \
     task.roll_aug=true \
     optimization.max_update=${max_update} \
--- SSLAM_Finetune/SSLAM_Ft_AS2M/scripts/finetuning_AS2M.sh
+++ SSLAM_Finetune/SSLAM_Ft_AS2M/scripts/finetuning_AS2M.sh
@@ -11,10 +11,9 @@ warmup_update=40000
 
 num_gpus=1
 
-mnt_vol=/mnt/fast/nobackup/users/ta01123/
-main_dir=${mnt_vol}/eat_apr30/fairseq/
+main_dir=/path/to/fairseq/
 echo "main_dir: $main_dir"
-eat_dirname=AS2M/EAT_FtCkptMixRegionExtraMixLCls1Layer2S2TDrpInpTBothNoMixClsAS2M
+eat_dirname=SSLAM_Ft_AS2M
 
 exp_dir_name=$(basename "$eat_dirname") ## take last part of eat_dirname
 
@@ -26,10 +25,10 @@ d_m_hh_mm_ss=$(date +"%d_%m_%H_%M_%S")
 
 log_file=${main_dir}/${eat_dirname}/experiments/${exp_dir_name}/${exp_dir_name}_${d_m_hh_mm_ss}_log.log
 
-pretrained_weight=/mnt/fast/nobackup/users/ta01123/eat_apr30/fairseq/EAT_CkptMixRegionExtraMixLCls1Layer2S2TDrpInpTBothNoMixCls/experiments/EAT_CkptMixRegionExtraMixLCls1Layer2S2TDrpInpTBothNoMixCls/checkpoint_last.pt
+pretrained_weight=/path/to/SSLAM_Stage2/experiments/SSLAM_Stage2/checkpoint_last.pt
 
 
-${mnt_vol}/eat_apr30/fairseqvenv/bin/python ${main_dir}/fairseq_cli/hydra_train.py -m \
+/path/to/fairseqvenv/bin/python ${main_dir}/fairseq_cli/hydra_train.py -m \
     --config-dir ${main_dir}/${eat_dirname}/config \
     --config-name finetuning  \
     common.user_dir=${eat_dirname} \
@@ -38,11 +37,11 @@ ${mnt_vol}/eat_apr30/fairseqvenv/bin/python ${main_dir}/fairseq_cli/hydra_train.
     checkpoint.restore_file=${main_dir}/${eat_dirname}/experiments/${exp_dir_name}/checkpoint_last.pt \
     checkpoint.best_checkpoint_metric=mAP \
     dataset.batch_size=${batch_size} \
-    task.data=${mnt_vol}/eat_data_manifest/manifest_as2mfull_ft \
+    task.data=${main_dir}/eat_data_manifest/manifest_as2mfull_ft \
     task.h5_format=false \
     task.num_samples=200000 \
     task.AS2M_finetune=true \
-    task.weights_file=${mnt_vol}/eat_data_manifest/manifest_as2mfull_ft/merged_unbal_bal_train_weka_weight.csv \
+    task.weights_file=${main_dir}/eat_data_manifest/manifest_as2mfull_ft/merged_unbal_bal_train_weka_weight.csv \
     task.target_length=1024 \
     task.roll_aug=true \
     optimization.lr=[$ft_lr] \
